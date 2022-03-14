ddd5b4168e060057a23d4384ba392f39bc46e713
Alex Beaty <macarrao_1@protonmail.ch>
2022-03-13 22:22:13 -0500

    Filter songs with BS, remove dependency PyAutoGui

--- SC-reposts-scraper.py
+++ SC-reposts-scraper.py
@@ -16,19 +16,17 @@ from typing import NamedTuple
 from os import path as Path
 from pathlib import PurePath
 from bs4 import BeautifulSoup
-import requests
 import sys
 
 Point = NamedTuple('Point', [('x',float),('y',float)])
 
 def extract(url: str):
-    elem = None
+    elem = ''
     # uncomment for headless
     # fireFoxOptions = webdriver.FirefoxOptions()
     # fireFoxOptions.set_headless()
     # driver = webdriver.Firefox(firefox_options=fireFoxOptions)
     driver = webdriver.Firefox()
-    driver.implicitly_wait(10)
     driver.get(url)
     
     global windowLocation
@@ -48,35 +46,58 @@ def extract(url: str):
     #  compensation for window position, browser toolbar, tabs bar, etc.
     
     windowLocation = Point(windowLocation.x+1, windowLocation.y+1)
-    pag.moveTo(windowLocation.x,windowLocation.y)
     
-    pag.click()
-    pag.press('F11')
-
-    pag.moveTo(windowSize['width']/2,windowSize['height']/2)
-
+    first_art = WebDriverWait(driver, 10).until(
+                EC.visibility_of(
+                    driver.find_element(By.CLASS_NAME, "sound__coverArt")            
+                )
+            )
+    art_size = first_art.rect
+    
     try:
+        actions = ActionChains(driver)
+        actions.move_to_element_with_offset(first_art,art_size["width"] + 1, art_size["height"] + 1)
+        # actions.send_keys(Keys.F11)
+        actions.send_keys(Keys.PAGE_DOWN)
+        actions.perform()
+        
         scrollReposts(driver)
-        # Make a copy of relevant data, because Selenium will throw if
-        # you try to access the properties after the driver quit
-        path = download(url, '.')
+        # # Make a copy of relevant data, because Selenium will throw if
+        # # you try to access the properties after the driver quit
+        # page_source = driver.page_source
+        # path = save(page_source, '.')
+        time.sleep(3)
     finally:
         driver.close()
-        
-    songs = run(path)
 
-    for song in songs:
-        elem = elem + song + '\n'
+    # songs_array = run(path)
 
-    return elem
+    # for song in songs_array:
+    #     elem = elem + song + '\n'
+
+    # return elem
 
 def scrollReposts(driver: webdriver):
+    print("Scrolling")
     # delimiter of reposts page, signals bottom of reposts list
-    css_selector = ".paging-eof"
     # <div class="paging-eof sc-border-light-top" title=""></div>
+    css_selector = ".paging-eof"
     condition = True
     while condition:
-        pag.press('pagedown')
+        # cover_art = None
+        # art_size_x = 0
+        # art_size_y = 0
+        # while art_size_y <= 0:
+        #     cover_art = driver.find_element(By.CLASS_NAME, "sound__coverArt")
+        #     art_size = cover_art.rect
+        #     art_size_x = art_size["width"]
+        #     art_size_y = art_size["height"]
+
+        actions = ActionChains(driver)
+        # actions.move_to_element_with_offset(cover_art, art_size_x + 1, art_size_y + 1)
+        # actions.send_keys(Keys.F11)
+        actions.send_keys(Keys.PAGE_DOWN)
+        actions.perform()
         try:
             WebDriverWait(driver, 1).until(
                 EC.visibility_of_element_located(
@@ -88,25 +109,15 @@ def scrollReposts(driver: webdriver):
         else:
             condition = False
 
-def download(url, dir):
-    file_name = PurePath(url).name
+def save(text, dir):
+    file_name = PurePath(url).name + '.html'
     file_path = Path.join(dir, file_name)
-    text = ''
-    
-    try:
-        response = requests.get(url)
-        if response.ok:
-            text = response.text
-        else:
-            print('Bad response for', url, response.status_code)
-    except requests.exceptions.ConnectionError as exc:
-        print(exc)
         
     with open(file_path, 'w') as fh:
         fh.write(text)
     
     # debug
-    print("downloaded html:")
+    print("~~~~~~~~~~~~~downloaded html~~~~~~~~~~~~~")
     print(text)
     return(file_path)
     
@@ -118,20 +129,26 @@ def parse_html(path):
 
 def transform(soup: BeautifulSoup):
     songs = []
-    containers = soup.find_all(id='container')
-    for item in containers:
-        if item is not None:
-            songs.append(item.get_text())
+    div_repost_lazyList = soup.find("div", class_="userReposts lazyLoadingList")
+    ul_repost_list = div_repost_lazyList.contents[0]
+    
+    for repost_item in ul_repost_list.children:
+        if repost_item is not None:
+            coverArt_link = repost_item.find("a", class_="sound__coverArt")
+            song = coverArt_link.get('href')
+            songs.append(song)
     return songs
 
 def run(path):
     soup = parse_html(path)
     content = transform(soup)
-    unserialized = []
+    songs_list = []
     for song in content:
-        unserialized.append(song.strip() if song is not None else '')
+        stripped_song = song.strip() if song is not None else ''
+        full_url = "https://soundcloud.com" + stripped_song
+        songs_list.append(full_url)
 
-    return unserialized
+    return songs_list
 
 if __name__ == "__main__":
     # debug: test with random profile
@@ -142,9 +159,9 @@ if __name__ == "__main__":
 
     elem = extract(url)
     if elem is not None:
-        with open('reposts.txt', 'w') as fh:
+        with open('reposts-1.txt', 'w') as fh:
             fh.write(elem)
-        print("wrote songs to reposts.txt")
+        print("wrote songs to reposts-1.txt")
         print(elem)
     else:
         print("Sorry, could not extract data")
